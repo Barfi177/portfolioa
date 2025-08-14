997e2126866ecf07b911974aaf9003299128f049
Barfi177 <alabarfi99@gmail.com>
2025-08-14 15:18:10 +0600

    Add src/components/File9.jsx - Generated by Adorable AI

---
+++ src/components/File9.jsx
@@ -0,0 +1,12 @@
+import OrderForm from '../components/OrderForm';
+
+const OrdersPage = () => {
+  return (
+    <div>
+      <h1>Place an Order</h1>
+      <OrderForm />
+    </div>
+  );
+};
+
+export default OrdersPage;
