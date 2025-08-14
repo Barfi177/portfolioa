ae8e2668e42accc4e5f237c321ff45fa795a287a
Barfi177 <alabarfi99@gmail.com>
2025-08-14 15:18:02 +0600

    Add src/components/File2.jsx - Generated by Adorable AI

---
+++ src/components/File2.jsx
@@ -0,0 +1,13 @@
+import FoodCard from './FoodCard';
+
+const FoodList = ({ foods }) => {
+  return (
+    <div className="food-list">
+      {foods.map((food) => (
+        <FoodCard key={food.id} food={food} />
+      ))}
+    </div>
+  );
+};
+
+export default FoodList;
